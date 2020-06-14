6cf4c0c0c098da6e3dc30d742269931110f81a95
kouzukek <kouzukek@code>
2020-06-14 23:31:02 +0900

    fix workflow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn()
+  };
+});
+
+test("Only render", () => {
+  render(
     <Provider store={store}>
       <App />
     </Provider>
   );
-
-  expect(getByText(/learn/i)).toBeInTheDocument();
 });
