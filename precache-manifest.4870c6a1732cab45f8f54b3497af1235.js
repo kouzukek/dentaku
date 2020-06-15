4dd8c61af1e7e1fe9214d34c0fd85ed6c3578d59
kouzukek <kouzukek@users.noreply.github.com>
2020-06-15 10:20:37 +0000

    deploy: 2ce55df14b4fe70c8af5fd65ec0be477feaaee17

---
+++ precache-manifest.4870c6a1732cab45f8f54b3497af1235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcecc1fd21e7ea67763198545bb300f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1259c275c0676396e393",
+    "url": "/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "e56ea337288937148e59",
+    "url": "/static/css/main.b81aef37.chunk.css"
+  },
+  {
+    "revision": "1259c275c0676396e393",
+    "url": "/static/js/2.bd7473a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bd7473a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56ea337288937148e59",
+    "url": "/static/js/main.f303729b.chunk.js"
+  },
+  {
+    "revision": "768dfca8e2e2f9876265",
+    "url": "/static/js/runtime-main.043c610b.js"
+  }
+]);
